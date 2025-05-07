831334b6c4343e8115e3e121ca7e876a02f555ca
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-07 15:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	const regix=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._]+\.[a-zA-Z]{2,}$/
+	return regix.test(str)
 }
 
-// Do not change the code below.
+// // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
